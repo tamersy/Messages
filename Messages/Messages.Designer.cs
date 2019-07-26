3ea7e277b7566f8307e65db74cff08010b74086b
Tamer <tamersy>
2019-07-26 13:13:07 +0300

    Messages in C#

---
+++ Messages/Messages.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Messages
+{
+    partial class Messages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 41);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.button1.Location = new System.Drawing.Point(18, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show Message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message Type :";
+            // 
+            // Messages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(235, 154);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Messages";
+            this.Text = "Messages";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Messages/Messages.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Messages
+{
+    public partial class Messages : Form
+    {
+        public Messages()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Error");
+            comboBox1.Items.Add("Information");
+            comboBox1.Items.Add("Warning");
+            comboBox1.Items.Add("Question");
+            comboBox1.Items.Add("Exclamation");
+            comboBox1.Items.Add("Stop");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Error")
+            {
+                MessageBox.Show("Error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (comboBox1.Text == "Information")
+            {
+                MessageBox.Show("Information", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (comboBox1.Text == "Warning")
+            {
+                MessageBox.Show("Warning", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (comboBox1.Text == "Question")
+            {
+                MessageBox.Show("Question", "Question", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+            else if (comboBox1.Text == "Exclamation")
+            {
+                MessageBox.Show("Exclamation", "Exclamation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                MessageBox.Show("Stop", "Stop", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+    }
+}
